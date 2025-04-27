3af4dff2f1e52334422c943b87325661b8594cb6
thainguyen101b <thainguyen101b@gmail.com>
2025-04-28 04:08:45 +0700

    feat: add GlobalExceptionHandlerTest, ObjectMapperConfigurationTest, PasswordEncoderAdapterTest

---
+++ server/src/test/java/io/nguyen/jwtbasedauthserver/config/ObjectMapperConfigurationTest.java
@@ -0,0 +1,49 @@
+package io.nguyen.jwtbasedauthserver.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class ObjectMapperConfigurationTest {
+    ObjectMapper sut;
+
+    @BeforeEach
+    void setUp() {
+        sut = new ObjectMapper().registerModule(new ObjectMapperConfiguration().iso8601SerializeModule());
+    }
+
+    @SuppressWarnings("deprecation")
+    @Test
+    void canSerialize_LocalDateTime() {
+        assertTrue(sut.canSerialize(LocalDateTime.class));
+    }
+
+    @Test
+    void convert_ISO8601String_toLocalDateTime() {
+        // given
+        String iso8601String = "2021-08-01T00:00:00.000Z";
+
+        // when
+        LocalDateTime localDateTime = sut.convertValue(iso8601String, LocalDateTime.class);
+
+        // then
+        assertThat(localDateTime).isEqualTo(LocalDateTime.of(2021, 8, 1, 0, 0, 0, 0));
+    }
+
+    @Test
+    void convert_LocalDateTime_toISO8601String() {
+        // given
+        LocalDateTime localDateTime = LocalDateTime.of(2021, 8, 1, 0, 0, 0, 0);
+
+        // when
+        String iso8601String = sut.convertValue(localDateTime, String.class);
+
+        // then
+        assertThat(iso8601String).isEqualTo("2021-08-01T00:00:00.000Z");
+    }
+}
